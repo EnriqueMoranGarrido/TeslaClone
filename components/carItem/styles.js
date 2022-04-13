aec05a30f1e0bb92d826732518e44290f0cc865c
Enrique Moran <enrique.morango@udlap.mx>
2022-04-13 18:17:03 -0500

    Car Item Component

---
+++ components/carItem/styles.js
@@ -0,0 +1,32 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+
+    titles:{
+        marginTop:'30%',
+        width:'100%',
+        alignItems: 'center',
+      },
+      title:{
+        fontSize:40,
+        fontWeight:'800',
+      },
+      subtitle:{
+        fontSize:15,
+        color:'#5c5e62',
+      },
+      carContainer: {
+        width: '100%',
+        height: '100%',
+        alignItems: 'center',
+      },
+      image:{
+        height: '100%',
+        width: '100%',
+        resizeMode: 'cover',
+        position: 'absolute',
+      }
+
+});
+
+export default styles;
